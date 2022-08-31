2b93a0e791a084f80a857799d05364310da652ed
Alamin Khan <alamin@Alamins-Mini.lan>
2022-08-31 13:45:58 +0300

    database

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,16 +2,43 @@ import React from 'react'
 import { format } from 'date-fns'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
 
 const BookingModal = ({ treatments, date, setTreatments }) => {
     const { _id, name, slots } = treatments;
     const [user, loading, error] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
 
     const handleBookings = (event) => {
         event.preventDefault();
         const slot = event.target.slot.value;
         console.log(_id, name, slot);
-        setTreatments(null);
+        const booking = {
+            treatmentsId: _id,
+            treatments: name,
+            date: formattedDate,
+            slot,
+            patient: user.email,
+            patienName: user.displayName,
+            phone: event.target.phone.value,
+        }
+        fetch('http://localhost:8000/booking',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(booking)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.success){
+                    toast(`Appointment is set for , ${ formattedDate } at ${slot}`);
+                }
+                else{
+                    toast.error(`You already have an appointment , ${ data.booking?.date } at ${data.booking?.slot }`);
+                }
+                setTreatments(null);
+            })
     }
 
 
